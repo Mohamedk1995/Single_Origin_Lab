76ae704c1b0f1b844aa0342aba937c1c06845aba
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-17 16:37:20 +0100

    first commit

---
+++ src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -0,0 +1,40 @@
+package com.bnta.chocolate.controllers;
+
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("chocolates")
+public class ChocolateController {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Chocolate>> getAllChocolates(){
+        return new ResponseEntity<>(chocolateRepository.findAll(), HttpStatus.OK);
+    }
+
+    //SHOW
+    @GetMapping(value ="/{id}")// localhost:8080/chocolates/1
+    public ResponseEntity<Optional<Chocolate>> getChocolate(@PathVariable Long id){
+        return new ResponseEntity<>(chocolateRepository.findById(id), HttpStatus.OK);
+    }
+
+    //POST
+    @PostMapping // POST localhost:8080/chocolates
+    public ResponseEntity<Chocolate> createChocolate(@RequestBody Chocolate newChocolate) {
+        chocolateRepository.save(newChocolate);
+        return new ResponseEntity<>(newChocolate, HttpStatus.CREATED);
+    }
+
+
+}
